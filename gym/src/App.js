5ad1afafe5585910a0d7c013a4bd1eb59e258ce7
EdCastro5820 <46980919+EdCastro5820@users.noreply.github.com>
2019-03-28 18:11:06 -0600

    Add files via upload

--- gym/src/App.js
+++ gym/src/App.js
@@ -7,19 +7,27 @@ import './App.css';
 //Components
 
 // Generics
-import Navbar from './components/generic/navbar/navbar'
+import Navbar from './components/generic/navbar/navbar';
 import Footer from './components/generic/footer/Footer';
 
 
 //Pages
+import Login from './components/pages/login/Login';
+import Signup from './components/pages/signup/signup';
 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+
       <Navbar/>
+      <Login />
+      <Signup />
       <Footer/>
+
+      
       </div>
     );
   }
