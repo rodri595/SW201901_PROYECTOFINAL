cafed6b57f4834af8520b9175b78f00d67716774
gerytomeizaguirre <35714628+gerytomeizaguirre@users.noreply.github.com>
2019-04-05 13:50:02 -0600

    Add files via upload
    hot fix

--- INSERTNAMEHERE/routes/api/users.js
+++ INSERTNAMEHERE/routes/api/users.js
@@ -19,6 +19,29 @@ function initUsers(db){
         );
   }); // getAllThings
 
+router.post('/login', function(req, res, next){
+    var _userData = req.body;
+    if(req.body.email === "rodri.595@hotmail.com"
+        && req.body.password ==="baleadas") {
+            req.session.logged = true;
+            req.session.loggeduser = req.body.email;
+            res.status(200).json({"msg":"ok"});
+        } else {
+            res.status(403).json({"error":"Credenciales no válidas"});
+        }
+});// post login
+
+router.get('/logout', function (req, res, next) {
+    var _userData = req.body;
+    req.session.logged=false;
+    req.session.loggeduser = null;
+    res.json({ "msg": "ok" });
+});// post login
+
+router.get('/session', function (req, res, next) {
+  res.json({ "active": req.session.logged && true});
+});// post login
+
 
 
 
